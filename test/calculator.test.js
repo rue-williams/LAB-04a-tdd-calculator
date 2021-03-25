ab0babb8dc6c0e93d4916ab9fe69e8ad12b33fdf
Rue Williams <19MEDIAFOOD81@GMAIL.COM>
2021-03-25 12:00:01 -0700

    updated html and css, ran first test

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,16 +1,18 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add } from '../calculator.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('add function test', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 10;
+    const y = 3;
+    const expected = 13;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(x, y);
 
     //Expect
     // Make assertions about what is expected versus the actual result
